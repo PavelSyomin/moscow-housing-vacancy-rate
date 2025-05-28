57f3fdf3e100438a6d72f156093b9dd7685e2a0f
Pavel Syomin <pas.ntg@gmail.com>
2025-05-28 16:39:57 +0500

    Add data files, reorganise files and folders, remove extra code

---
+++ intercarto-intergis/scripts/extract_ce.py
@@ -0,0 +1,12 @@
+import pathlib
+
+import pandas as pd
+import tqdm
+
+
+data = []
+for fn in tqdm.tqdm(pathlib.Path("data/ce").glob("*")):
+    df = pd.read_excel(fn)
+    data.append(df[["КН", "Площадь"]].dropna())
+
+pd.concat(data).to_parquet("data/flats_area.parquet")
