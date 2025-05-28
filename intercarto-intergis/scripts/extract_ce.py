33c4615039862d206f4bc2753fe3f25b6a6df576
Pavel Syomin <pas.ntg@gmail.com>
2025-05-28 17:09:05 +0500

    Update paths in scripts

--- intercarto-intergis/scripts/extract_ce.py
+++ intercarto-intergis/scripts/extract_ce.py
@@ -5,8 +5,8 @@ import tqdm
 
 
 data = []
-for fn in tqdm.tqdm(pathlib.Path("data/ce").glob("*")):
+for fn in tqdm.tqdm(pathlib.Path("../data/ce").glob("*")):
     df = pd.read_excel(fn)
     data.append(df[["КН", "Площадь"]].dropna())
 
-pd.concat(data).to_parquet("data/flats_area.parquet")
+pd.concat(data).to_parquet("../data/flats_area.parquet")
